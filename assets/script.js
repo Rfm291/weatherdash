866678f511813c2172e1e9dd27b15b6e298ef36a
Richard <79869790+Rfm291@users.noreply.github.com>
2021-08-21 11:36:00 -0400

    Initial code w/ modifications

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// Global variables
+var SearchHistory = [];
+var weatherApiUrl = 'https://api.openweathermap.org';
+var weatherApiKey = '14ccf524b8658a56a69592d112c92c7f';
+
+var searchForm = document.querySelector('#search-form');
+var searchInput = ('#seach-input')
+var TodaysContainer = ('#today')
+var searchHistoryContainer =('#forecast')
+var forecastContainer = ('#history')
+
+//Display history
+function displaySearchHistory()
+
+//Updates history to local storage
+function appendHistory(search)
+
+//Function to get search history from local storage
+function grabSearchHistory ()
